4114a2914924bf29b7223b8bb2a197df159af6a5
Christina Cornell <cdhorn515@gmail.com>
2017-08-29 15:05:50 -0400

    added map model and controller

---
+++ src/main/java/com/cdhorn/Controllers/MapController.java
@@ -0,0 +1,7 @@
+package com.cdhorn.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MapController {
+}
---
+++ src/main/java/com/cdhorn/Models/Map.java
@@ -0,0 +1,71 @@
+package com.cdhorn.Models;
+
+import javax.persistence.*;
+
+@Entity
+public class Map {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private boolean isPublic = false;
+    private String startPosition;
+    private String endPosition;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "run_id")
+    private Run run;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public String getStartPosition() {
+        return startPosition;
+    }
+
+    public void setStartPosition(String startPosition) {
+        this.startPosition = startPosition;
+    }
+
+    public String getEndPosition() {
+        return endPosition;
+    }
+
+    public void setEndPosition(String endPosition) {
+        this.endPosition = endPosition;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Run getRun() {
+        return run;
+    }
+
+    public void setRun(Run run) {
+        this.run = run;
+    }
+}
