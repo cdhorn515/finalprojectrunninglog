f1b6e399aa72b4f2f7d908620319d3806cd7492b
Christina Cornell <cdhorn515@gmail.com>
2017-08-29 13:23:06 -0400

    CRUD functionality for user runs is complete

--- src/main/java/com/cdhorn/Controllers/UserController.java
+++ src/main/java/com/cdhorn/Controllers/UserController.java
@@ -7,12 +7,14 @@ import com.cdhorn.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.sql.Date;
+import java.sql.Time;
 
 
 @Controller
@@ -49,22 +51,50 @@ public class UserController {
         String username = principal.getName();
         User user = userRepo.findByUsername(username);
         Run userRun = new Run();
+        String time = hour + ":" + minute + ":" + second;
         userRun.setDate(date);
         userRun.setDistance(distance);
         userRun.setHour(hour);
         userRun.setMinute(minute);
         userRun.setSecond(second);
-//        Time runTime = Time.valueOf(time);
-//        System.out.println(runTime);
-//        userRun.setTime(runTime);
+        Time runTime = Time.valueOf(time);
+        System.out.println(runTime);
+        userRun.setTime(runTime);
         userRun.setUser(user);
         runRepo.save(userRun);
         return "addRun";
     }
 
-    @RequestMapping("/user/updateRun")
-    public String updateRun() {
-
+    @RequestMapping(value = "/user/updateRun/{id}", method = RequestMethod.GET)
+    public String updateRun(@PathVariable("id") long id,
+                            Model model) {
+        Run run = runRepo.findOne(id);
+        model.addAttribute("run", run);
         return "editRun";
+    }
+
+    @RequestMapping(value = "/user/updateRun/{id}", method = RequestMethod.POST)
+    public String updateRun(@PathVariable("id") long id,
+                            @RequestParam("date") String date,
+                            @RequestParam("distance") float distance,
+                            @RequestParam("time") String time,
+                            Model model) {
+        Run userRun = runRepo.findOne(id);
+
+        userRun.setDistance(distance);
+        Date runDate = Date.valueOf(date);
+        Time runTime = Time.valueOf(time);
+//        System.out.println(runTime);
+        userRun.setDate(runDate);
+        userRun.setTime(runTime);
+        runRepo.save(userRun);
+        model.addAttribute(userRun);
+        return "redirect:/user";
+    }
+
+    @RequestMapping(value = "/user/deleteRun", method = RequestMethod.POST)
+    public String deleteRun(@RequestParam("id") long id) {
+        runRepo.delete(id);
+        return "redirect:/user";
     }
 }
