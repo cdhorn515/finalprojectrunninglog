c575f7df4bf46f0e281f363e7b6544001f8ce3b8
Christina Cornell <cdhorn515@gmail.com>
2017-09-02 01:14:27 -0400

    getting information back from api request, still need to create classes for information that will be used. map template able to render google map with polyline path from api response

---
+++ src/main/java/com/cdhorn/GoogleMaps/ApiDirections.java
@@ -0,0 +1,16 @@
+package com.cdhorn.GoogleMaps;
+
+import com.google.api.client.json.GenericJson;
+
+public class ApiDirections extends GenericJson{
+
+
+    private String start_address;
+    private double latitude;
+    private double longitude;
+    private String end_address;
+    private String overview_polyline;
+
+
+
+}
---
+++ src/main/java/com/cdhorn/Models/ApiKey.java
@@ -0,0 +1,11 @@
+package com.cdhorn.Models;
+
+
+public class ApiKey {
+
+     final String EMBED_API_KEY = "AIzaSyDIsWaPj6AZqT_7auXdVezHgMaS6KKifvM";
+     final String DIRECTIONS_API_KEY = "AIzaSyDilpvAx_fYKFMZaFiwobLqYder6PCeKv8";
+     final String STATIC_MAPS_API_KEY = "AIzaSyCecE50bCCZBpd3m3sLbDP-oGEXC7MUdz0";
+
+
+}
