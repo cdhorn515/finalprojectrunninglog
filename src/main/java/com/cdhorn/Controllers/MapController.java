537537ee706d976a98b72354506f057b45f01f2d
Christina Cornell <cdhorn515@gmail.com>
2017-09-05 21:43:44 -0400

    able to pass map url variable to html template and display on endpoint

--- src/main/java/com/cdhorn/Controllers/MapController.java
+++ src/main/java/com/cdhorn/Controllers/MapController.java
@@ -2,6 +2,7 @@ package com.cdhorn.Controllers;
 
 import com.cdhorn.GoogleMaps.ApiDirectionsFeed;
 import com.cdhorn.GoogleMaps.ApiKey;
+import com.cdhorn.GoogleMaps.ApiStaticMap;
 import com.cdhorn.GoogleMaps.Routes;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,11 +23,8 @@ import java.util.List;
 @Controller
 public class MapController {
 
-
     @RequestMapping("/map")
     public String testMap(Model model) throws IOException, ClassCastException {
-
-
 
         ApiKey apiKey = new ApiKey();
         System.out.println(apiKey.getDIRECTIONS_API());
@@ -36,7 +34,6 @@ public class MapController {
         HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet("https://maps.googleapis.com/maps/api/directions/json?origin=furman+university+greenville+SC&destination=swamp+rabbit+grocery+greenville+sc&key=" + apiKey.getDIRECTIONS_API() + "&mode=walking");
         HttpResponse response = client.execute(request);
-
 
         System.out.println(response);
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -54,33 +51,32 @@ public class MapController {
         ApiDirectionsFeed feed = mapper.readValue(line, ApiDirectionsFeed.class);
         System.out.println("----------------------------------------------------");
         System.out.println("");
-        //System.out.println(feed.getRoutes().get(0).getOverviewPolylineObject().getPolyline());
         List<Routes> x = feed.getRoutes();
         Routes myRoute = mapper.convertValue(feed.getRoutes().get(0), new TypeReference<Routes>() {});
-        //Routes myRoute = x.get(0);
         LinkedHashMap polylineObject = myRoute.getOverviewPolylineObject();
         Object myPoints = polylineObject.get("points");
         System.out.println(myPoints);
+        String polylineString = myPoints.toString();
+//        System.out.println(polylineString);
 
-        // System.out.println(feed.getRoutes().get(0).get("overview_polyline").getPoints());
-        // System.out.println(feed.getRoutes().get(0).get("overview_polyline").get("points"));
-        // System.out.println(feed.getRoutes().get(0).containsKey("overview_polyline"));
-//        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-//        for (Routes routes : feed.getRoutes()) {
-//            System.out.println(routes.getOverviewPolylineObject().getPolyline());
-//        }
+        String polylineStringReformatted = polylineString.replace("|", "%7C");
+        System.out.println(polylineStringReformatted);
 
+        Object polylineObjectReformatted = polylineStringReformatted;
+        System.out.println("===================================================");
+        System.out.println("");
+//        System.out.println(polylineObjectReformatted);
 
+        ApiStaticMap apiStaticMap = new ApiStaticMap();
+        String url = apiStaticMap.getStaticMapUrl();
+        String startMarker = apiStaticMap.getStartMarkerParameters();
+        String finishMarker = apiStaticMap.getFinishMarkerParameters();
+        String pathParams = apiStaticMap.getPathParameters();
+        String staticMapApiKeyParams = apiStaticMap.getStaticMapApiKey();
+         url += startMarker + "34.9195746,-82.4217151" + finishMarker + "34.8701836,-82.4217151" + staticMapApiKeyParams + apiKey.getSTATIC_MAP_API() + pathParams + polylineStringReformatted;
 
-        //below throws java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to com.cdhorn.GoogleMaps.Routes
-//        for (Routes routes : feed.getOverviewPolylineObject()) {
-//            System.out.println(routes.getOverviewPolylineObject().getPolyline());
-//        }
-
-//        String replacePipe = apiUrl.replace("|", "%7C");
-//            System.out.println(replacePipe);
-//        String mapUrl = replacePipe.replace("//", "/");
-//        model.addAttribute("mapUrl", mapUrl);
+        System.out.println(url);
+        model.addAttribute("url", url);
         return "map";
 
     }
--- src/main/java/com/cdhorn/GoogleMaps/ApiStaticMap.java
+++ src/main/java/com/cdhorn/GoogleMaps/ApiStaticMap.java
@@ -3,15 +3,50 @@ package com.cdhorn.GoogleMaps;
 
 public class ApiStaticMap {
 
-    private String staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap?zoom=10&amp;size=300x300";
+    private String staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=100x100";
     private String startMarkerOrigin; //use latitude from directions response
     private String finishMarkerDestination; //use longitude from directions response
     private String overviewPolyline; //use overview polyline from directions response
-    private String startMarkerParameters = "&amp;markers=size:mid%7Ccolor:green%7Clabel:S%7C";
-    private String finishMarkerParameters = "&amp;markers=size:mid%7Ccolor:red%7Clabel:F%7C";
-    private String pathParamters = "&amp;path=color:blue%7Cenc:";
-    private String staticMapApiKey = "&amp;key=";
+    private String startMarkerParameters = "&markers=size:mid%7Ccolor:green%7Clabel:S%7C";
+    private String finishMarkerParameters = "&markers=size:mid%7Ccolor:red%7Clabel:F%7C";
+    private String pathParameters = "&path=color:blue%7Cenc:";
+    private String staticMapApiKey = "&key=";
 
     private ApiDirections apiDirections;
 
+    public String getStaticMapUrl() {
+        return staticMapUrl;
+    }
+
+    public String getStartMarkerOrigin() {
+        return startMarkerOrigin;
+    }
+
+    public String getFinishMarkerDestination() {
+        return finishMarkerDestination;
+    }
+
+    public String getOverviewPolyline() {
+        return overviewPolyline;
+    }
+
+    public String getStartMarkerParameters() {
+        return startMarkerParameters;
+    }
+
+    public String getFinishMarkerParameters() {
+        return finishMarkerParameters;
+    }
+
+    public String getPathParameters() {
+        return pathParameters;
+    }
+
+    public String getStaticMapApiKey() {
+        return staticMapApiKey;
+    }
+
+    public ApiDirections getApiDirections() {
+        return apiDirections;
+    }
 }
