2e26c40b07c15ad717a89a6c8a9ad31155dabcca
Christina Cornell <cdhorn515@gmail.com>
2017-09-14 18:57:07 -0400

    removed enable async from main, did not end up using

--- src/main/java/com/cdhorn/FinalprojectApplication.java
+++ src/main/java/com/cdhorn/FinalprojectApplication.java
@@ -2,10 +2,8 @@ package com.cdhorn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class FinalprojectApplication {
 
 	public static void main(String[] args) {
